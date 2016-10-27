65cb45436d7491941ad54bcfd775d4474dcf117a
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-27 18:31:55 +0530

    Add v2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to v1</h2>
+          <h2>Welcome to v2</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
